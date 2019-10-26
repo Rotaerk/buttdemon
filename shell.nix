be7be92b37fdbb1f16d392043255648285137d09
Matthew Stewart <m.scott.stewart@gmail.com>
2019-10-25 23:46:25 -0400

    Initial commit.

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+let
+  inherit (lib) makeLibraryPath;
+  hs = haskell.packages.ghc865;
+  tools = [
+    (hs.ghcWithPackages (ps: [ps.shake]))
+    hs.cabal-install
+    hs.ghcid
+  ];
+  libraries = [
+    assimp
+  ];
+  libraryPath = "${makeLibraryPath libraries}";
+in
+  pkgs.runCommand "shell" {
+    buildInputs = tools ++ libraries;
+    shellHook = ''
+      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${libraryPath}"
+      export LIBRARY_PATH="${libraryPath}"
+    '';
+  } ""
